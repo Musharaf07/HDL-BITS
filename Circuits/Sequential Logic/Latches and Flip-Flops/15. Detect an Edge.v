4da31151a2f4da8383b05ee0893c682ff680317c
Musharaf07 <146569624+Musharaf07@users.noreply.github.com>
2025-07-01 09:17:58 +0530

    Update 15. Detect an Edge.v

--- Circuits/Sequential Logic/Latches and Flip-Flops/15. Detect an Edge.v
+++ Circuits/Sequential Logic/Latches and Flip-Flops/15. Detect an Edge.v
@@ -1,15 +1,11 @@
 module top_module (
     input clk,
-    input j,
-    input k,
-    output Q); 
-    
-    always @(posedge clk) begin
-        case ({j,k})
-            2'b00 : Q <= Q;
-            2'b01 : Q <= 0;
-            2'b10 : Q <= 1;
-            2'b11 : Q <= ~Q;
-        endcase
+    input [7:0] in,
+    output [7:0] pedge
+);
+    reg [7:0] temp;
+    always@(posedge clk) begin
+        temp <= in;
+        pedge <= ~temp & in;
     end
 endmodule
