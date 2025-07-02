1c59291cc6f680c7ae54cc3006d0f538d24d2992
Musharaf07 <146569624+Musharaf07@users.noreply.github.com>
2025-07-02 10:20:04 +0530

    Create 18. Dual-Edge Triggered Flip-Flop.v

---
+++ Circuits/Sequential Logic/Latches and Flip-Flops/18. Dual-Edge Triggered Flip-Flop.v
@@ -0,0 +1,16 @@
+module top_module (
+    input clk,
+    input d,
+    output q
+);
+    reg r0, r1;
+    
+    always @(posedge clk)
+        r0 <= d;
+    
+    always @(negedge clk)
+        r1 <= d;
+    
+    assign q = clk ? r0 : r1; 
+
+endmodule
